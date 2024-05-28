65cf4641ab0ee8838cb2ec467044de513182d20f
Kevin Diaz <kevrocks67@gmail.com>
2024-05-28 00:59:52 -0400

    Initial code commit
    - Write/Read golden artifact to Consul

---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	capi "github.com/hashicorp/consul/api"
+)
+
+type Artifact struct {
+	ArtifactType       string `json:"artifact_type"`
+	ArtifactID         string `json:"artifact_id"`
+	PromotionTimestamp string `json:"promotion_timestamp"`
+}
+
+func getGoldenArtifact(c *gin.Context) {
+	var goldenArtifact Artifact
+	artifactType := c.Param("artifact_type")
+
+	client, err := capi.NewClient(capi.DefaultConfig())
+	if err != nil {
+		panic(err)
+	}
+
+	kv := client.KV()
+
+	pair, _, err := kv.Get(fmt.Sprintf("artifacts/golden/%s", artifactType), nil)
+	if err != nil {
+		panic(err)
+	}
+
+	json.Unmarshal(pair.Value, &goldenArtifact)
+
+	c.IndentedJSON(http.StatusOK, goldenArtifact)
+}
+
+func promoteGoldenArtifact(c *gin.Context) {
+	var newGoldenArtifact Artifact
+	artifactType := c.Param("artifact_type")
+
+	if err := c.BindJSON(&newGoldenArtifact); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Bad Request"})
+	}
+
+	t := time.Now().UTC()
+	newGoldenArtifact.PromotionTimestamp = t.Format(time.RFC3339)
+
+	client, err := capi.NewClient(capi.DefaultConfig())
+	if err != nil {
+		panic(err)
+	}
+	kv := client.KV()
+
+	newGoldenArtifactJSON, err := json.Marshal(newGoldenArtifact)
+	if err != nil {
+		panic(err)
+	}
+
+	p := &capi.KVPair{Key: fmt.Sprintf("artifacts/golden/%s", artifactType), Value: newGoldenArtifactJSON}
+	_, err = kv.Put(p, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	c.IndentedJSON(http.StatusCreated, newGoldenArtifact)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/golden/:artifact_type", getGoldenArtifact)
+	router.POST("/golden/:artifact_type", promoteGoldenArtifact)
+
+	router.Run("0.0.0.0:8080")
+}
