e20acaa03b9f9716c074883510454ce4d8c7853f
Kevin Diaz <kevrocks67@gmail.com>
2024-08-25 23:52:19 -0400

    Refactor provider config

--- internal/api/models/provider.go
+++ internal/api/models/provider.go
@@ -19,17 +19,4 @@ func InitProvider(providerType string, config *providers.ProviderConfig) {
 		}
 		Provider = p
 	}
-
-	/*
-		if providerType == "test" {
-			p := &test.TestClient{}
-
-			//TODO read from json file
-			err := p.Init(*config)
-
-			if err != nil {
-				panic(err)
-			}
-			Provider = p
-		}*/
 }
